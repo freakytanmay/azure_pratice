7441dfb94b9bb4fad6fbe6bece90599a2f67fe0f
Tanmay Dhandhukiya <93569427+freakytanmay@users.noreply.github.com>
2022-04-12 20:30:57 +0530

    hello java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+public static void main(string args[]){
+    System.out.println("hello ddu");
+    System.out.println("tanmay");
+    System.out.println("ma052");
+    
+
+
+}
+}
